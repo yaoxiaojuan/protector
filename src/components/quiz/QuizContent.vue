d7b695472d9241a70f770191baf151ee81ce5bc8
Yao Xiaojuan <yaoxiaojuan@cloudin.cn>
2017-07-22 21:40:54 +0800

    数据更新至正常状态

--- src/components/quiz/QuizContent.vue
+++ src/components/quiz/QuizContent.vue
@@ -21,7 +21,7 @@
       v-on:click="handlePage('front')"
     >下一页</button>
     <button
-      v-if="page === 0"
+      v-if="page === 4"
       class="button small"
       v-on:click="handleSubmit"
     >完成</button>
@@ -38,35 +38,35 @@ export default {
       quizs: {
         0: [
           {title: '我觉得闷闷不乐，情绪低沉。', type: 0, result: null},
-          {title: '我觉得一天之中早晨最好。', type: 1, result: null}
-          // {title: '我一阵阵哭出来或觉得想哭。', type: 0, result: null},
-          // {title: '我晚上睡眠不好。', type: 0, result: null},
-          // {title: '我吃的跟平成一样多。', type: 1, result: null}
+          {title: '我觉得一天之中早晨最好。', type: 1, result: null},
+          {title: '我一阵阵哭出来或觉得想哭。', type: 0, result: null},
+          {title: '我晚上睡眠不好。', type: 0, result: null},
+          {title: '我吃的跟平成一样多。', type: 1, result: null}
+        ],
+        1: [
+          {title: '我与异性亲密接触时和以往一样感觉愉快。', type: 1, result: null},
+          {title: '我发觉我的体重在下降。', type: 0, result: null},
+          {title: '我有便秘的苦恼。', type: 0, result: null},
+          {title: '我心跳比平时快。', type: 0, result: null},
+          {title: '我无缘无故地感到疲乏。', type: 0, result: null}
+        ],
+        2: [
+          {title: '我的头脑跟平常一样清楚。', type: 1, result: null},
+          {title: '我觉得经常做的事情并没有困难。', type: 1, result: null},
+          {title: '我觉得不安而平静不下来。', type: 0, result: null},
+          {title: '我觉得经常做的事情并没有困难。', type: 1, result: null},
+          {title: '我对将来抱有希望。', type: 0, result: null}
+        ],
+        3: [
+          {title: '我比平常容易生气激动。', type: 1, result: null},
+          {title: '我觉得作出决定是容易的。', type: 1, result: null},
+          {title: '我觉得自己是个有用的人，有人需要我。', type: 1, result: null},
+          {title: '我的生活过得很有意思。', type: 0, result: null},
+          {title: '我认为如果我死了别人会生活得好些。', type: 1, result: null}
+        ],
+        4: [
+          {title: '平常感兴趣的事我仍然照样感兴趣。', type: 0, result: null}
         ]
-        // 1: [
-        //   {title: '我与异性亲密接触时和以往一样感觉愉快。', type: 1, result: null},
-        //   {title: '我发觉我的体重在下降。', type: 0, result: null},
-        //   {title: '我有便秘的苦恼。', type: 0, result: null},
-        //   {title: '我心跳比平时快。', type: 0, result: null},
-        //   {title: '我无缘无故地感到疲乏。', type: 0, result: null}
-        // ],
-        // 2: [
-        //   {title: '我的头脑跟平常一样清楚。', type: 1, result: null},
-        //   {title: '我觉得经常做的事情并没有困难。', type: 1, result: null},
-        //   {title: '我觉得不安而平静不下来。', type: 0, result: null},
-        //   {title: '我觉得经常做的事情并没有困难。', type: 1, result: null},
-        //   {title: '我对将来抱有希望。', type: 0, result: null}
-        // ],
-        // 3: [
-        //   {title: '我比平常容易生气激动。', type: 1, result: null},
-        //   {title: '我觉得作出决定是容易的。', type: 1, result: null},
-        //   {title: '我觉得自己是个有用的人，有人需要我。', type: 1, result: null},
-        //   {title: '我的生活过得很有意思。', type: 0, result: null},
-        //   {title: '我认为如果我死了别人会生活得好些。', type: 1, result: null}
-        // ],
-        // 4: [
-        //   {title: '平常感兴趣的事我仍然照样感兴趣。', type: 0, result: null}
-        // ]
       },
       result: 0,
       tips: false
